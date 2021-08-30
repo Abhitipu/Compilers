32227eddc7c133b4da403a5248be31cd68bd1e75
Abhitipu <abhinandan0316@gmail.com>
2021-08-31 01:03:42 +0530

    Added float part

--- Assn2/ass2_19CS10069.c
+++ Assn2/ass2_19CS10069.c
@@ -108,6 +108,7 @@ int readFlt(float *f) {
 	
 	int mxn = 66;
 	char temp[mxn];
+
 	__asm__ __volatile__(
 		"movl $0, %%eax \n\t"
 		"movq $1, %%rdi \n\t"
@@ -115,6 +116,42 @@ int readFlt(float *f) {
 		:
 		:"S"(temp), "d"(mxn)
 	);
+
+	int integralPart = 0, integral = 1;
+	float fractionalPart = 0.0, multiplier = 0.1;
+
+	int ptr = 0, neg = 0;
+	if(temp[ptr] == '-') {
+		neg = 1;
+		++ptr;
+	} else if(temp[ptr] == '+') {
+		++ptr;
+	} else if(temp[ptr] < '0' || temp[ptr] > '9') {
+		return ERR;
+	}
+
+	for(; temp[ptr] != '\0' && temp[ptr] != ' ' && temp[ptr] != '\n'; ptr++) {
+		if(integral) {
+			if(temp[ptr] == '.') {
+				integral = 0;
+			} else if(temp[ptr] <= '9' && temp[ptr] >= '0') {
+				integralPart = integralPart*10 + (temp[ptr] - '0');
+			} else {
+				return ERR;
+			}
+		} else {
+			if(temp[ptr] <= '9' && temp[ptr] >= 0) {
+				fractionalPart += (temp[ptr] - '0')*multiplier;
+				multiplier *= 0.1;
+			} else {
+				return ERR;
+			}
+		}
+	}
+
+	*f = (float)integralPart + fractionalPart;
+	if(neg)
+		*f = -(*f);
 	return OK;
 }
 
@@ -123,14 +160,46 @@ int printFlt(float f) {
 	// Need to print sign for -ve number
 	// Decimal point needed in all cases to distinguish from int
 	// Return : Success ? no of chars printed : Err
-	int mxn = 66;
+	int mxn = 66, digAfterDecimal = 8;
 	char temp[mxn];
+	int ptr = 0, neg = 0;
+	
+	if(f < 0) {
+		neg = 1;
+		temp[ptr] = '-';
+		++ptr;
+		f = -f;
+	}
+
+	int integralPart = (int)f;
+	int dig = 0;
+	int m = integralPart;
+	for(; m > 0; dig++)
+		m /= 10;
+	
+	m = integralPart;
+	ptr += dig;
+	for(; dig > 0; dig--) {
+		temp[dig-1+neg] = (char)(m%10 + '0');
+		m /= 10;
+	}
+	
+	temp[ptr++] = '.';
+	f -= (float)integralPart;
+	float fNew = f;
+	
+	for(int i = 0; i < digAfterDecimal; i++) {
+		fNew *= 10;
+		int curVal = (int)fNew;
+		temp[ptr++] = (char)(curVal%10 + '0');
+	}
+	
 	__asm__ __volatile__(
 		"movl $1, %%eax \n\t"
 		"movq $1, %%rdi \n\t"
 		"syscall \n\t"
 		:
-		:"S"(temp), "d"(mxn)
+		:"S"(temp), "d"(ptr)
 	);
-	return mxn;
+	return ptr;
 }

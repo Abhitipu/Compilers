7a3b4350f3a2f53e336b49ce0b476bd50a8db76f
Aryan Singh <ary1733@gmail.com>
2021-09-10 07:16:31 +0000

    Files arranged

---
+++ Assn3/ass3_19CS10069_19CS30007.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    FILE *fp = fopen("ass3_19CS10069_19CS30007_test.c", "r");
+    yyin = fp;
+    yylex();
+    fclose(fp);
+    return 0;
+}
---
+++ Assn3/ass3_19CS10069_19CS30007_test.c
@@ -0,0 +1,8 @@
+int main()
+{
+    int a, b;
+
+    printf("asda");
+
+    return 0;
+}

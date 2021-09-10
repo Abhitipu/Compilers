54465b6f8f313060777b8ddcf25a83024ed0046f
Aryan Singh <ary1733@gmail.com>
2021-09-10 13:42:06 +0000

    Part of Constants

--- Assn3/ass3_19CS10069_19CS30007.c
+++ Assn3/ass3_19CS10069_19CS30007.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-extern FILE* yyin;
+
 extern int yylex();
 int main()
 {
-    FILE *fp = fopen("ass3_19CS10069_19CS30007_test.c", "r");
-    yyin = fp;
     yylex();
-    fclose(fp);
     return 0;
 }

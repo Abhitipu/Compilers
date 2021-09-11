e1eb33134033a10fe0921620b914bca7afc75e98
Abhitipu <abhinandan0316@gmail.com>
2021-09-11 17:18:20 +0530

    Added some tests | added '\0' escape sequence

--- Assn3/ass3_19CS10069_19CS30007_test.c
+++ Assn3/ass3_19CS10069_19CS30007_test.c
@@ -1,51 +1,39 @@
-// C program to Open a File,
-// Write in it, And Close the File
-/*
-	A multiline comment
-	Test it!//
-*/
-# include <stdio.h>
-# include <string.h>
+#include<stdio.h>
 
-int main( )
-{
+int main() {
 
-	// Declare the file pointer
-	FILE *filePointer ;
-	const float dummy_float_int = 12.43E-14;
-	// Get the data to be written in file
-	char dataToBeWritten[50]
-		= "GeeksforGeeks-A Computer Science Portal for Geeks\'";
+    // Lets test some data types first
+    int n;
+    n = 34;
+    float f;
+    f = 3.4e-12;
+    char c;
+    c = 'X';
+    c = '-';     
 
-	// Open the existing file GfgTest.c using fopen()
-	// in write mode using "w" attribute
-	filePointer = fopen("GfgTest.c", "w") ;
-	
-	// Check if this filePointer is null
-	// which maybe if the file does not exist
-	if ( filePointer == NULL )
-	{
-		printf( "GfgTest.c file failed to open." ) ;
-	}
-	else
-	{
-		
-		printf("The file is now opened.\n") ;
-		
-		// Write the dataToBeWritten into the file
-		if ( strlen ( dataToBeWritten ) > 0 )
-		{
-			
-			// writing in the file using fputs()
-			fputs(dataToBeWritten, filePointer) ;
-			fputs("\n", filePointer) ;
-		}
-		
-		// Closing the file using fclose()
-		fclose(filePointer) ;
-		
-		printf("Data successfully written in file GfgTest.c\n");
-		printf("The file is now closed.") ;
-	}
+    /*
+     * Here we will be exploring some strings
+     * We'll do these things:
+     * 1. We'll convert a string to upper case.
+     * 2. Then we'll convert it back to lower case.
+     */
+    
+    char str[] = "Oh dear! When will this online sem end?!";
+    // Lets convert the string tp upper case
+    for(register int i = 0; str[i] != '\0'; i++) {
+        if(str[i] <= 'z' && str[i] >= 'a')
+            str[i] -= 32;
+    }
+
+    // Lets go back to lower case using a while loop now
+    register int i = 0;
+    while(str[i] != '\0') {
+        if(str[i] <= 'Z' && str[i] >= 'A')
+            str[i] += 32;
+        i++;
+    }
+    
+    char nullChar = '\0';
+    
 	return 0;	
 }

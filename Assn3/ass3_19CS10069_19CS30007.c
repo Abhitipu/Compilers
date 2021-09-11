96553e79088e2dd9aadbdf4211e3877e48014c68
Aryan Singh <ary1733@gmail.com>
2021-09-11 09:41:54 +0000

    Print format

--- Assn3/ass3_19CS10069_19CS30007.c
+++ Assn3/ass3_19CS10069_19CS30007.c
@@ -4,36 +4,37 @@ int main() {
     int token;
     while (token = yylex()) {
         switch (token) {
-            case KEYWORD:                   printf("KEYWORD = %s\n", yytext); 
+            case KEYWORD:                   printf("<KEYWORD, %d, %s>\n", token, yytext); 
                                             break;
-            case IDENTIFIER:                printf("IDENTIFIER = %s\n", yytext); 
+            case IDENTIFIER:                printf("<IDENTIFIER, %d, %s>\n", token, yytext); 
                                             break;
-            case FLOATING_CONST:            printf("FLOATING_CONST = %s\n", yytext); 
+            case FLOATING_CONST:            printf("<FLOATING_CONST, %d, %s>\n", token, yytext); 
                                             break;
-            case INTEGER_CONST:             printf("INTEGER_CONST = %s\n", yytext); 
+            case INTEGER_CONST:             printf("<INTEGER_CONST, %d, %s>\n", token, yytext); 
                                             break;
-            case CHARACTER_CONST:           printf("CHARACTER_CONST = %s\n", yytext); 
+            case CHARACTER_CONST:           printf("<CHARACTER_CONST, %d, %s>\n", token, yytext); 
                                             break;
-            case ESCAPE_SEQUENCE:           printf("ESCAPE_SEQUENCE = %s\n", yytext); 
+            case ESCAPE_SEQUENCE:           printf("<ESCAPE_SEQUENCE, %d, %s>\n", token, yytext); 
                                             break;
-            case STRING_LITERAL:            printf("STRING_LITERAL = %s\n", yytext); 
+            case STRING_LITERAL:            printf("<STRING_LITERAL, %d, %s>\n", token, yytext); 
                                             break;
-            case PUNCTUATOR:                printf("PUNCTUATORS = %s\n", yytext); 
+            case PUNCTUATOR:                printf("<PUNCTUATOR, %d, %s>\n", token, yytext); 
                                             break;
-            case BEGIN_SINGLE_COMMENT:      printf("BEGIN_SINGLE_COMMENT = %s", yytext); 
-                                            break;
-            case END_SINGLE_COMMENT:        printf("\nEND_SINGLE_COMMENT\n"); 
+            case BEGIN_SINGLE_COMMENT:      printf("<BEGIN_SINGLE_COMMENT, %d, %s>\n", token, yytext); 
                                             break;
             case SINGLE_COMMENT:            printf("%s", yytext); 
                                             break;
-            case BEGIN_MULTI_COMMENT:       printf("BEGIN_MULTI_COMMENT = %s", yytext); 
+            case END_SINGLE_COMMENT:        printf("\n<END_SINGLE_COMMENT, %d, >\n",token); 
+                                            break;
+            case BEGIN_MULTI_COMMENT:       printf("<BEGIN_MULTI_COMMENT, %d, %s>\n", token, yytext); 
                                             break;
             case MULTI_COMMENT:             printf("%s", yytext); 
                                             break;
-            case END_MULTI_COMMENT:         printf("\nEND_MULTI_COMMENT\n"); 
+            case END_MULTI_COMMENT:         printf("\n<END_MULTI_COMMENT, %d, %s>\n", token, yytext); 
                                             break;
             case WHITE_SPACE:               break;
             default:                        break;
         }
     }
+    return 0;
 }

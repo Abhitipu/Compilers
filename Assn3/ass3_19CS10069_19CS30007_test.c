9467ede8d6231f749021a2ef921e014c31ca41be
Abhitipu <abhinandan0316@gmail.com>
2021-09-15 19:50:00 +0530

    Increased test coverage

--- Assn3/ass3_19CS10069_19CS30007_test.c
+++ Assn3/ass3_19CS10069_19CS30007_test.c
@@ -7,12 +7,32 @@ typedef struct student {
     float gpa;
 } stud;
 
-enum MyNumber {
+union MyNumber {
     short mySmallNumber;
     int myNumber;
     long myBigNumber;
     float myRealNumber;
+    double myDoubleumber;
+    _Complex myComplexNumber;
+    _Bool myBooleanValue;
+    _Imaginary myImaginaryValue;
 };
+
+enum week{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday    
+};
+
+extern int check;
+
+inline int add(int x, int y) {
+    return x + y;
+}
 
 int main() {
 
@@ -25,10 +45,22 @@ int main() {
     c = 'X';
     c = '-';     
     
+    // testing a switch statement 
+    const int k = 2;
+    switch (k) {
+        case 1: printf("1\n")
+        case 2: printf("2\n")
+        case 3: printf("3\n")
+        case 4:                                      
+        case 5: printf("45\n")
+                break;              
+        default : printf("6\n")
+    }
+
     // lets count the odd nos in an array
     int arr[] = {1, 0, 123, 100000007};
     int ctr = 0;
-    for(int i = 0; i < 4; i++) {
+    for(auto i = 0; i < 4; i++) {
         if(arr[i]%2 == 0) {
             continue;
         } else {
@@ -36,6 +68,7 @@ int main() {
         }
     }
     
+    printf("%d\n", sizeof(arr));   
     // lets compute the sum of the real nos
     float arrf[] = {0.1212, -1.3e-13, 1.2E24, 1E10, 20};
     float ans = 0.0;
@@ -67,9 +100,12 @@ int main() {
         i++;
     }
     
+    goto newLabel;
+
     char nullChar = '\0';
     
     // lets create some instances of students
+    newLabel:
     stud student1, student2;
 
     student1.name = "Tom";

fa62348e285a907777880c219b29b10b940879e8
Aryan Singh <ary1733@gmail.com>
2021-09-28 11:54:02 +0000

    Debug Enum

--- Assn4/asgn4_19CS10069_19CS30007.c
+++ Assn4/asgn4_19CS10069_19CS30007.c
@@ -2,18 +2,19 @@
 #include "y.tab.h"
 
 extern int yyparse();
-
+// yyparse() returns a value of 0 if the input it parses is valid according to the given grammar rules. It returns a 1 if the input is incorrect and error recovery is impossible
 int main() 
 {
     extern int yydebug;
     yydebug = 1;
 
-    int works = yyparse();
+    int error = yyparse();
 
-    if(works) {
-        printf("Done!\n");
-    } else {
+    if(error) {
         printf("Error!\n");
     }
+    else {
+        printf("Done!\n");
+    } 
     return 0;
 }
